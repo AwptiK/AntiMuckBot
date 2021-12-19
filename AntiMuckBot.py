2cfd253e7ad2b06b21c79efbcaa58446fa11bca4
AwptiK <70664258+AwptiK@users.noreply.github.com>
2021-12-19 23:27:28 +0200

    Add files via upload

--- AntiMuckBot.py
+++ AntiMuckBot.py
@@ -24,54 +24,59 @@ logfileName = "AlreadyRepliedToComments.txt"
 characterWhitelist = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 
+# Add the comment ID to a text file just in case it somehow wants to reply to it again. Which should NEVER happen
 def AddCommentIDToList(comment_id):
     file = open(logfileName, "a")
     file.write(comment_id)
     file.write("\n")
     file.close()
 
-
+# Shut whoever is trying to Muck Chain up
 def TryToShutMucker():
-    reddit = praw.Reddit(
+    reddit = praw.Reddit(           ####################
         client_id=clientID,
-        client_secret=clientSecret,
+        client_secret=clientSecret, #Connect to Reddit#
         password=password,
-        user_agent=userAgent,
+        user_agent=userAgent,      ####################
         username=username,
     )
-    print(Fore.YELLOW + reddit.user.me().__str__())
+    print(Fore.YELLOW + reddit.user.me().__str__()) # Print your username to verify the connections
 
-    desiredSub = reddit.subreddit(subName)
+    desiredSub = reddit.subreddit(subName) # Choose the sub you want to destroy
 
-    print(Fore.LIGHTBLUE_EX + "We online bois")
-    print(Fore.LIGHTBLUE_EX + "Connected to subreddit of name: " + Fore.YELLOW + desiredSub.display_name)
+    print(Fore.LIGHTBLUE_EX + "We online bois") # more verification
+    print(Fore.LIGHTBLUE_EX + "Connected to subreddit of name: " + Fore.YELLOW + desiredSub.display_name) # even more verification
 
     for comment in desiredSub.stream.comments(skip_existing=True):
+        # Check if comment is already replied to. Helps when testing, not in actual use
         with open(logfileName) as logFile:
             if comment.id in logFile.read():
                 print(Fore.YELLOW + "Comment has already been replied to")
                 continue
 
+        # convert all text to lower case for ease of use
         commentBody = comment.body.lower()
+        # remove any symbols and weird shit
         commentBodyLettersOnly = ''.join(filter(characterWhitelist.__contains__, commentBody))
-        # if commentBodyLettersOnly.lower().__contains__() in muckVariations:
-        #     comment.reply(muckReply + replyBody)
-        #     AddCommentIDToList(comment.id)
-        #     print(Fore.GREEN + "Mucker has been shut up")
+        # Loop over every muck variation and check if the comment contains one of them
         for muckVariation in bannedWords:
             if muckVariation in commentBodyLettersOnly:
                 comment.reply(muckReply + replyBody)
                 AddCommentIDToList(comment.id)
                 print(Fore.GREEN + "Mucker has been shut up")
 
-
+# Constantly run this shit
 while True:
+    # Try to shut a mucker up
     try:
         TryToShutMucker()
 
+    # If the user presses Control and C, exit the program/stop the bot
     except KeyboardInterrupt:  # Ctrl + C exits
         print(Fore.RED + "Exiting...")
         break
+
+    # if error, tell error
     except Exception as error:  # What did you do idiot?
         print(Fore.RED + "Well...fuck, something done fucked up")
         print(Fore.RED + "Error is: " + error.__str__())
--- topSecretInfo.py
+++ topSecretInfo.py
@@ -1,6 +1,14 @@
+#Visit reddit.com/prefs/apps and create a new a *script* app
+
+# The client ID of your app. Right below the name, a bunch of characters and numbers
 clientID = ""
+#Should be below the client ID, also a bunch of characters and numbers
 clientSecret = ""
+# Your reddit username
 username = ""
+# Your reddit password. obviously don't share this 
 password = ""
+# The name of the subreddit 
 subName = ""
+# A unique and descriptive identifier for your bot. Something like: "Anti Muck bot by u/{USERNAME}"
 userAgent = ""
